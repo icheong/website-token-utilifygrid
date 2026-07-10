3e2a4dc4110bf709c02411a04217d6fb93aa6949
icheong <i.cheong@gmail.com>
2026-07-10 10:43:34 +1000

    chore: downgrade Astro dependencies, integrate @astrojs/tailwind, and update Wrangler build configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   integrations: [
     react(),
+    tailwind(),
   ],
 
   vite: {
